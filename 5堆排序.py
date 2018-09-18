5dace8c4a6ae5fa22caaec31a93ebc0793d898a7
srp527 <244356095@qq.com>
2018-09-18 15:58:18 +0800

    update

--- 5堆排序.py
+++ 5堆排序.py
@@ -14,7 +14,8 @@ __author__ = 'SRP'
 其实我们的堆排序算法就是抓住了堆的这一特点，每次都取堆顶的元素，将其放在序列最后面，
 然后将剩余的元素重新调整为最大堆，依次类推，最终得到排序的序列。'''
 
-import random,time
+import random,time,math
+
 list2 = [random.randrange(10000) for i in range(10000)]
 list = [15,2,5,9,8,3,4,52,45,98,67,50,35,14,27,21,5]
 
@@ -48,10 +49,37 @@ def sort(list,root,end):
         global count
         count += 1
 
+#打印树的一个函数，很好用，谁用谁知道
+def print_tree(array): #打印堆排序使用
+     '''
+     深度 前空格 元素间空格
+     1     7       0
+     2     3       7
+     3     1       3
+     4     0       1
+     '''
+     index = 1
+     depth = math.ceil(math.log2(len(array))) # 因为补0了，不然应该是math.ceil(math.log2(len(array)+1))
+     sep = '  '
+     for i in range(depth):
+         offset = 2 ** i
+         print(sep * (2 ** (depth - i - 1) - 1), end='')
+         line = array[index:index + offset]
+         for j, x in enumerate(line):
+             print("{:>{}}".format(x, len(sep)), end='')
+             interval = 0 if i == 0 else 2 ** (depth - i) - 1
+             if j < len(line) - 1:
+                 print(sep * interval, end='')
+         index += offset
+         print()
+
+print_tree(list)
+print('-'*60)
 t1 = time.time()
-heap_sort(list2)
+heap_sort(list)
 t2 = time.time()
-print(list2,t2-t1,count)
+print_tree(list)
+print(list,t2-t1,count)
 
 
 # count = 0
